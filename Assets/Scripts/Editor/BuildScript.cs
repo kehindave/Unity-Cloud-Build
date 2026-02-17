ac0332fb5c2b3b6fb39e100b298fa8ab68cf81ea
kehindave <kehinday3@gmail.com>
2026-02-17 14:56:05 +0100

    Updated build configs and logic

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -5,9 +5,19 @@ public class BuildScript
     public static void BuildWindows()
     {
         BuildPipeline.BuildPlayer(
-            new[] { "Assets/Scenes/Test Scene" },
+            new[] { "Assets/Scenes/Test Scene.unity" },
             "Build/Windows/Game.exe",
             BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+    }
+    
+    public static void BuildMac()
+    {
+        BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/Test Scene.unity" },
+            "Build/mac/Game.exe",
+            BuildTarget.StandaloneOSX,
             BuildOptions.None
         );
     }
